0019d81cbd058ec0acd892079378577aa25502f2
ivandasigan <51710349+ivandasigan@users.noreply.github.com>
2021-12-14 11:22:19 +0800

    Added Soldier

--- POP.playground/Contents.swift
+++ POP.playground/Contents.swift
@@ -35,5 +35,36 @@ class Bishop: GameUnit {
     }
 }
 
+
+struct Soldier: GameUnit {
+    var id: String
+    
+    var isAlive: Bool = true
+    
+    var healthLevel: Int = 3
+    
+    init(name: String) {
+        id = name
+    }
+    mutating func hitTaken() {
+        if healthLevel > 0 {
+            healthLevel-=1
+            if healthLevel == 0 {
+                isAlive = false
+            }
+        } else {
+            isAlive = false
+        }
+        let state = isAlive ? String(repeating: "♥️", count: healthLevel) : "🚀"
+        print("\(id): Arghh! \(state)")
+    }
+    
+    
+}
+
+
 var bishop = Bishop(name: "Xander")
-bishop.hitTaken()
+var soldier = Soldier(name: "Bean")
+soldier.hitTaken()
+soldier.hitTaken()
+soldier.hitTaken()
